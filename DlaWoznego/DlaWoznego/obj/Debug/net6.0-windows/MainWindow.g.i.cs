464676090cb4006ada51518fd2ce5cc767c240e3
Alaliszon121 <isabelle.prince.umh@gmail.com>
2023-01-14 00:57:24 +0100

    Zmiana wyglądu buttona

--- DlaWoznego/DlaWoznego/obj/Debug/net6.0-windows/MainWindow.g.i.cs
+++ DlaWoznego/DlaWoznego/obj/Debug/net6.0-windows/MainWindow.g.i.cs
@@ -117,7 +117,7 @@ namespace DlaWoznego {
                 return;
             }
             _contentLoaded = true;
-            System.Uri resourceLocater = new System.Uri("/DlaWoznego;component/mainwindow.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/DlaWoznego;V1.0.0.0;component/mainwindow.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\MainWindow.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
